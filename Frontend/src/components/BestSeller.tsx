73449917c44800e2636439b2e69f2fbb31efe148
Lodhi123-ux <ammar.lodhi@manhattandatainc.com>
2025-10-26 18:50:27 +0500

    Minor updates

--- Frontend/src/components/BestSeller.tsx
+++ Frontend/src/components/BestSeller.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import type { ProductType } from "../assets/assets";
 import Title from "./Title";
